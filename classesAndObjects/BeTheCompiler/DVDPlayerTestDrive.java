da714be10b36732f97b0582a121acf7f78fab074
HP <chaitanyapk21@gmail.com>
2020-05-18 19:17:19 +0530

    space given

--- classesAndObjects/BeTheCompiler/DVDPlayerTestDrive.java
+++ classesAndObjects/BeTheCompiler/DVDPlayerTestDrive.java
@@ -11,7 +11,7 @@ class DVDPlayer{
 public class DVDPlayerTestDrive {
   public static void main(String[] args){
     DVDPlayer d = new DVDPlayer();
-    d.canRecord =  true;
+    d.canRecord = true;
     d.playDVD();
   
     if(d.canRecord == true){
